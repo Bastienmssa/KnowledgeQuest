7ac78297535d72796235d0e0a02cbbcc5ae1d326
Massa <bastien.massa@esme.fr>
2025-03-18 14:15:29 +0100

    Mise en place des fichiers

---
+++ FrontEnd/src/api.js
@@ -0,0 +1,4 @@
+/*
+Contient des fichiers pour gérer les appels API ou les interactions avec des services externes.
+Cela peut inclure des fichiers pour configurer Axios ou Fetch pour les requêtes HTTP.
+*/
---
+++ FrontEnd/src/index.js
@@ -0,0 +1,4 @@
+/*
+Le point d'entrée JavaScript de l'application. Il est responsable du rendu de l'application React dans le DOM.
+Il contient généralement le code pour rendre le composant App dans un élément racine du DOM (souvent <div id="root"></div>).
+*/
